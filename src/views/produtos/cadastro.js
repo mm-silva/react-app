141f7e1432370cb662379637cf2308b025b53656
mm-silva <m.meneses@uni9.edu.br>
2020-05-12 17:06:49 -0300

    Views home and cadastro with routes

---
+++ src/views/produtos/cadastro.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class CadastroProduto extends React.Component {
+	render(){
+	   return (
+		<div className="card">
+			<div className="card-header">
+				Cadastro de Produtos
+			</div>
+			<div className="card-body">
+				
+			</div>
+		</div>
+         	);
+	}
+
+
+}
+
+
+export default CadastroProduto;
